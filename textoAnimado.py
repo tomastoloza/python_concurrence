f5cb5f15cae572bb668661e232e6c4ad76041d42
tomastoloza <tomasezequieltoloza@gmail.com>
2021-04-16 15:14:09 -0300

    Subidos ejercicios

--- textoAnimado.py
+++ textoAnimado.py
@@ -1,29 +1,35 @@
-#Importa módulos para Interfaz Gráfica de usuario (tkinter)
+# Importa módulos para Interfaz Gráfica de usuario (tkinter)
+import logging
+import multiprocessing
+import threading
 import tkinter as tk
 from tkinter import ttk
 import time
 
-#Crea la ventana principal
+# Crea la ventana principal
 main_window = tk.Tk()
 main_window.title("Ejemplo")
 main_window.configure(width=350, height=200)
 
-#Función que crea y posiciona el botón "Salir"
+
+# Función que crea y posiciona el botón "Salir"
 def opcionFinalizar():
     boton = ttk.Button(main_window, text="Salir", command=main_window.destroy)
     boton.place(x=170, y=170)
 
-#Función que crea una etiqueta (label) de texto en la posición (x,y) de la pantalla.
-def createLabel(a,b):
+
+# Función que crea una etiqueta (label) de texto en la posición (x,y) de la pantalla.
+def createLabel(a, b):
     label = ttk.Label(text="")
-    label.place(x=a,y=b)
+    label.place(x=a, y=b)
     return label
 
-#Función que crea una etiqueta (llamando a createLabel()) y luego anima texto dentro de la misma.
+
+# Función que crea una etiqueta (llamando a createLabel()) y luego anima texto dentro de la misma.
 def crearAnimacion(a, b, char):
     mylabel = createLabel(a,b)
     texto=""
-    retardo: float=0.25
+    retardo: float=0.05
     for i in range(0,35):
         time.sleep(retardo)
         texto += char
@@ -31,15 +37,37 @@ def crearAnimacion(a, b, char):
         main_window.update_idletasks()
         main_window.update()
 
-#Ejecuta tres animaciones
-crearAnimacion(10,10, 'X')
-crearAnimacion(10,30, 'Y')
-crearAnimacion(10,50, 'Z')
 
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+# Ejecuta tres animacionescon threads
+def ejecutar_animacion_threads():
+    thread_1 = threading.Thread(target=crearAnimacion, args=(10, 10, 'X'))
+    thread_2 = threading.Thread(target=crearAnimacion, args=(10, 30, 'Y'))
+    thread_3 = threading.Thread(target=crearAnimacion, args=(10, 50, 'Z'))
+    thread_1.start()
+    thread_2.start()
+    thread_3.start()
+
+# Ejecuta tres animaciones con procesos
+def ejecutar_animacion_procesos():
+    worker_1 = multiprocessing.Process( name='print x',target=crearAnimacion, args=(10, 10, 'X',))
+    worker_2 = multiprocessing.Process( name='print y',target=crearAnimacion, args=(10, 30, 'Y'))
+    worker_3 = multiprocessing.Process( name='print z',target=crearAnimacion, args=(10, 50, 'Z'))
+    worker_1.start()
+    worker_2.start()
+    worker_3.start()
+
+# ejecutar_animacion_threads()
+ejecutar_animacion_procesos()
 
 # Mantener las siguientes líneas siempre al final del script y en el mismo orden.
-#Coloca la opcion "Salir"
+# Coloca la opcion "Salir"
 opcionFinalizar()
 
-#Bucle principal de la ventana
+# Bucle principal de la ventana
 main_window.mainloop()
+
